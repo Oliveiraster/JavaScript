aa54f1abc5d708736e84bdb26217c57e6c190d48
Raphael Oliveira <116489080+Oliveiraster@users.noreply.github.com>
2023-05-17 13:52:23 -0300

    Condições com JS

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`A Velocidade do seu carro é ${vel} km/h.`)
+if (vel> 60){
+    console.log('Você ultrapassou a velocidade permitira. Multado!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
