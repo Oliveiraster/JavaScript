9c4fae254f80c48c6da3ed48d27d19efebf81fc2
Raphael Oliveira <116489080+Oliveiraster@users.noreply.github.com>
2023-05-19 00:16:37 -0300

    Conhecendo FOR em JS

--- Aula13/index.js
+++ Aula13/index.js
@@ -8,3 +8,8 @@ do{
     console.log(`passo ${x}`)
     x++
 } while(x < 5)
+
+var f = 1
+for (f=1; f<=10; f++){
+    console.log(`com ${f}FOR`)
+}
---
+++ Aula14/index.js
@@ -0,0 +1,11 @@
+var c = 1
+
+for(c=1; c<=3;c++){
+    console.log(`${c}`)
+}
+
+console.log('Vai começar ...')
+for (var t = 1;t <= 4; t++){
+    console.log(`${t}`)
+}
+console.log('FIM')
