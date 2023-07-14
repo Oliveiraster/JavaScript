97ee98d930f278ae7856a53f8525237d49e6143a
Raphael Oliveira <116489080+Oliveiraster@users.noreply.github.com>
2023-07-14 10:25:05 -0300

    revisando Js

---
+++ Hora de codar - JS/3_particularidades_js/index.js
@@ -0,0 +1,11 @@
+console.log('Primeiro')
+
+console.log('ultimo')
+
+console.log('Mais um console.log')
+
+var a = 1
+
+a = "teste"
+
+console.log(a)
---
+++ Hora de codar - JS/4_variaveis/scripts.js
@@ -0,0 +1,34 @@
+var teste = 1
+
+var nome = "João"
+console.log(teste)
+
+teste = "Matheus"
+console.log(teste)
+console.log(nome)
+
+var $nome = 'ASD'
+var _nome = 'ASD2'
+console.log($nome)
+console.log(_nome)
+
+var nome5 = 'ASD3'
+
+var meuPrimeiroNome = 'Raphael'
+console.log(meuPrimeiroNome) // camelcase
+
+var meusobrenome = 'Oliveira'
+console.log(meusobrenome)
+
+let testando = 1
+const ola = 2
+
+console.log(testando)
+console.log(ola)
+
+
+var meuNome
+console.log(meuNome)
+
+meuNome = 'Raphael Oliveira'
+console.log(meuNome)
