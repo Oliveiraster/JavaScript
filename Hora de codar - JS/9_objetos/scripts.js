35411e2affc9e94b97ef2889a22972ce3c1bef9e
Raphael Oliveira <116489080+Oliveiraster@users.noreply.github.com>
2023-07-14 14:53:32 -0300

    objeto

---
+++ Hora de codar - JS/9_objetos/scripts.js
@@ -0,0 +1,21 @@
+var obj ={
+    nome: 'Raphael',
+    idade: 30,
+    profissao: 'Desenvolvedor'
+}
+
+console.log(obj)
+console.log(typeof obj)
+
+console.log(obj.nome)
+console.log(obj.idade)
+console.log(obj.profissao)
+
+console.log(`O meu nome é ${obj.nome}`)
+
+obj.nome = 'José'
+console.log(obj.nome)
+
+console.log(obj)
+obj.home ='Apartamento'
+console.log(obj)
