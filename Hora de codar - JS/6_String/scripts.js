6b6e1f3477fc95c47d3469a4f621b5b508dbe43e
Raphael Oliveira <116489080+Oliveiraster@users.noreply.github.com>
2023-07-14 13:12:28 -0300

    String

---
+++ Hora de codar - JS/6_String/scripts.js
@@ -0,0 +1,13 @@
+var nome = 'Raphael'
+var sobrenome = 'Oliveira'
+
+var nomeCompleto = `${nome} ${sobrenome}`
+console.log(nomeCompleto)
+console.log(typeof nome)
+
+var frase = 'Esta é uma frase complexa'
+
+console.log(frase)
+console.log(typeof frase)
+ 
+document.write(`Este número: ${nome}`) // para escrever no HTML
