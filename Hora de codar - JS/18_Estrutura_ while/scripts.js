ade7d1aa33f400a3e902e864e38ce9d6bf3db8ea
Raphael Oliveira <116489080+Oliveiraster@users.noreply.github.com>
2023-07-18 07:41:00 -0300

    estrutura de repetição

---
+++ Hora de codar - JS/18_Estrutura_ while/scripts.js
@@ -0,0 +1,15 @@
+lista = ["Raphael", "Gerlucia" , "Afonso", "Aurilene"]
+
+var i = 0
+
+while(i < lista.length){
+    console.log(lista[i])
+    i++
+}
+
+var palavra = 'Raphael'
+y = 0
+while(y < palavra.length){
+    console.log(palavra[y])
+    y++
+}
