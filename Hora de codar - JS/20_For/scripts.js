c3e0c825c08284c72c3d7992684c13058897d5a2
Raphael Oliveira <116489080+Oliveiraster@users.noreply.github.com>
2023-07-20 07:13:22 -0300

    repetição com for

---
+++ Hora de codar - JS/20_For/scripts.js
@@ -0,0 +1,14 @@
+for(var i = 0; i <= 10; i++){
+    console.log(`Repetindo for: ${i}`)
+}
+
+var arr = [10,20,30,40,50,60]
+
+for(var j = 0; j <= arr.length; j++){
+    console.log(arr[j])
+}
+console.log(arr)
+
+for(var x =5; x <100; x*=3){
+    console.log(x)
+}
