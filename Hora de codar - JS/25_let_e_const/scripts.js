0c98cb98fe6d564f6efe84f50eadd5de90d2404a
Raphael Oliveira <116489080+Oliveiraster@users.noreply.github.com>
2023-07-20 13:27:22 -0300

    let const e metodos

---
+++ Hora de codar - JS/25_let_e_const/scripts.js
@@ -0,0 +1,27 @@
+let x = 5 //var x = 5
+console.log(x)
+
+const y = 10
+console.log(y)
+
+x = 12
+console.log(x)
+
+if(true){
+    let x = 20
+    console.log(x)
+    
+    const y = 50 
+    console.log(`Const if ${y}`)
+}
+
+console.log(x)
+
+if(20>10){
+    const y = 100
+    console.log(`const if 3 ${y}`)
+}
+
+for(let x = 0; x < 10; x++){
+    console.log(x)
+}
---
+++ Hora de codar - JS/26_metodos_numericos/scripts.js
@@ -0,0 +1,22 @@
+//parseFloat
+console.log(parseFloat('12.999'))
+console.log(Number.parseFloat('12.999'))
+
+//ParseInt
+console.log(parseInt('10'))
+console.log(parseInt(16.5))
+console.log(parseInt('16.5'))
+
+//tofixed
+console.log(23.457451.toFixed(2))
+
+//isNaN => isto é não é um numero ?!?!
+console.log(isNaN('teste')) 
+console.log(isNaN(12))
+console.log(isNaN('14'))
+
+//MAX_VALUE e MIN_VALUE
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+
+console.log(2.7976931348623157e+308)
